c8318105203bfa79a6f574b69710e4d45ced48cd
mahdi-mim <mahdiroozbahani25@gmail.com>
2024-05-15 10:54:58 -0700

    Added menu toggle

--- day-09/scripts/menu-toggle.js
+++ day-09/scripts/menu-toggle.js
@@ -1,4 +1,9 @@
 // Main Navigation Toggle
+const button = document.getElementById("main-nav-button");
+const links = document.getElementById("main-nav-list");
 
+button.addEventListener('click', function() {
+    links.classList.toggle('toggled');
+})
 
 // Thumb Navigation Toggle
